1731ebe9921d744d8f38247f6618de17615d2c9d
Michael Harris <mhguitar1997@gmail.com>
2019-08-12 13:16:18 -0700

    Create new repo for java countries

---
+++ src/main/java/com/javacountries/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.javacountries.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
