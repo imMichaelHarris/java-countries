4ccb22d51f755caad1f29fcecbd383fc6c7a3faf
Michael Harris <mhguitar1997@gmail.com>
2019-08-12 14:07:07 -0700

    Sort request scaffold

--- src/main/java/com/javacountries/demo/NamesController.java
+++ src/main/java/com/javacountries/demo/NamesController.java
@@ -3,6 +3,7 @@ package com.javacountries.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,10 @@ public class NamesController {
         return new ResponseEntity<>(DemoApplication.countryList.countryList, HttpStatus.OK);
     }
     // /names/start/{letter} - return alphabetically that start with letter
+    @GetMapping(value = "/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> sortByLetter(@PathVariable char letter){
+        return new ResponseEntity<>();
+    }
 
     // /names/size/{number} - eturn the countries alphabetically that have a name equal to or longer than the given length
 }
