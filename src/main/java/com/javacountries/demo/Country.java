f9712373c845473cabaeaa726f3b62ccfb935cec
Michael Harris <mhguitar1997@gmail.com>
2019-08-12 13:33:28 -0700

    Generate getters and setters for country

--- src/main/java/com/javacountries/demo/Country.java
+++ src/main/java/com/javacountries/demo/Country.java
@@ -5,4 +5,36 @@ public class Country {
     private int population;
     private double landMass;
     private int medianAge;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public double getLandMass() {
+        return landMass;
+    }
+
+    public void setLandMass(double landMass) {
+        this.landMass = landMass;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
 }
