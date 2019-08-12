cae632a30b89cf3d2bcc576535eb232073a55195
Michael Harris <mhguitar1997@gmail.com>
2019-08-12 14:03:23 -0700

    Get all names request functional

--- src/main/java/com/javacountries/demo/NamesController.java
+++ src/main/java/com/javacountries/demo/NamesController.java
@@ -1,4 +1,21 @@
 package com.javacountries.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/names")
 public class NamesController {
+
+    // /names/all - return names alphabetically
+    @GetMapping(value = "/all", produces = {"application/json"})
+    public ResponseEntity<?> getAllCountries(){
+        return new ResponseEntity<>(DemoApplication.countryList.countryList, HttpStatus.OK);
+    }
+    // /names/start/{letter} - return alphabetically that start with letter
+
+    // /names/size/{number} - eturn the countries alphabetically that have a name equal to or longer than the given length
 }
